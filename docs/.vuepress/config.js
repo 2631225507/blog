5767efbbaace53af67e2a6e00ac7ba8892521bf0
huanghegen <huanghg@one-sports.com>
2022-11-10 10:48:49 +0800

    feat(配置和文件):新增页面

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    themeConfig: {
+        nav: [
+            { text: '首页', link: '/' },
+            {
+                text: '博客',
+                items: [
+                    { text: 'GitHub', link: 'https://github.com/2631225507/blog.git' },
+                    { text: '线上地址', link: 'https://blog.csdn.net/weixin_44155115' }
+                ]
+            }],
+        // 侧边栏配置
+        sidebar: [
+            {
+                title: "基础知识",
+                path: '/note/uniapp', // 默认激活的选项
+                collapsable: false, // 不折叠
+                children: [
+                    { title: "uni-app", path: "/note/uniapp" },
+                    { title: "vue3", path: "/note/vue3" }
+                ],
+            }
+        ]
+    }
+}
