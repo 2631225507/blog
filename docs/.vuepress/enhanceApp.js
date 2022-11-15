baa5611bba4ed7ddcf1d7514fc1a2427ca6860b0
huanghegen <huanghg@one-sports.com>
2022-11-15 10:00:16 +0800

    1

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,16 +1,24 @@
-// import ElementUI from 'element-ui';
-// import 'element-ui/lib/theme-chalk/index.css';
-
-// export default async ({
-//   Vue
-// }) => {
-//   if (typeof process === 'undefined') {
-//     Vue.use(ElementUI)
-//   }
-// }
-
 // import components from './config/component'
 
 // // export default ({ Vue }) => {
 // //     components.forEach(({name, component}) => Vue.component(name, component))
 // // }
+
+/**
+ * 扩展 VuePress 应用
+ */
+
+ import Element from 'element-ui'
+ import 'element-ui/lib/theme-chalk/index.css'
+ 
+//  import './public/css/index.css' //组件css文件
+ 
+ export default ({
+   Vue, // VuePress 正在使用的 Vue 构造函数
+   options, // 附加到根实例的一些选项
+   router, // 当前应用的路由实例
+   siteData // 站点元数据
+ }) => {
+   // ...做一些其他的应用级别的优化
+   Vue.use(Element)
+ }
