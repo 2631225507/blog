e8ab99b2f9616a32e55da7295c2d25b8bc1e1a37
huanghegen <huanghg@one-sports.com>
2022-11-15 09:30:34 +0800

    feat()

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,16 +1,16 @@
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
+// import ElementUI from 'element-ui';
+// import 'element-ui/lib/theme-chalk/index.css';
 
-export default async ({
-  Vue
-}) => {
-  if (typeof process === 'undefined') {
-    Vue.use(ElementUI)
-  }
-}
-
-import components from './config/component'
-
-// export default ({ Vue }) => {
-//     components.forEach(({name, component}) => Vue.component(name, component))
+// export default async ({
+//   Vue
+// }) => {
+//   if (typeof process === 'undefined') {
+//     Vue.use(ElementUI)
+//   }
 // }
+
+// import components from './config/component'
+
+// // export default ({ Vue }) => {
+// //     components.forEach(({name, component}) => Vue.component(name, component))
+// // }
