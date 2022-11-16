5cd340261b3ae249eee8e5632eea8a359f2c8695
huanghegen <huanghg@one-sports.com>
2022-11-16 11:05:36 +0800

    feat(部署到github)

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:huanghegen/blog.git main:gh-pages
+git push -f git@github.com:2631225507/blog.git main:gh-pages
 
 cd -
